d9148f0136f065dcfa454aba41823bc5f04479d4
alexandergf <alexgferreiro@hotmail.com>
2018-10-04 13:28:18 +0200

    Version 1.3 que llama al json via web a traves de internet

--- app/src/main/java/com/alexandergf/userprofile/Profile.java
+++ app/src/main/java/com/alexandergf/userprofile/Profile.java
@@ -3,6 +3,14 @@ package com.alexandergf.userprofile;
 public class Profile {
     private String name, lastname, handle, following, followers, about;
 
+    public Profile(String name, String lastname, String handle, String following, String followers, String about) {
+        this.name = name;
+        this.lastname = lastname;
+        this.handle = handle;
+        this.following = following;
+        this.followers = followers;
+        this.about = about;
+    }
 
     public String getName() {
         return name;
--- app/src/main/java/com/alexandergf/userprofile/UserProfileActivity.java
+++ app/src/main/java/com/alexandergf/userprofile/UserProfileActivity.java
@@ -6,9 +6,20 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,34 +40,36 @@ public class UserProfileActivity extends AppCompatActivity {
     private ImageView fotoview;
     private TextView handleview;
     private TextView namelastview;
+    private RequestQueue mQueue;
 
+    private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
+        gson = new Gson();
+
+        /*
         try {
             InputStream stream = getAssets().open("AlexanderGonzalez.json");
             InputStreamReader reader = new InputStreamReader(stream);
-            Gson gson = new Gson();
             profile = gson.fromJson(reader, Profile.class);
         } catch (IOException e) {
             Toast.makeText(this, "No he pogut llegir", Toast.LENGTH_SHORT).show();
         }
+        */
 
+        //------------------------------------------------------
+        mQueue= Volley.newRequestQueue(this);
+        jsonParse();
+        //-------------------------------------------------------
         textabview = findViewById(R.id.textoabview);
         followingnumview=findViewById(R.id.followingnumview);
         followersnumview=findViewById(R.id.followersnumview);
         namelastview=findViewById(R.id.namelastview);
         handleview=findViewById(R.id.handleview);
-
-
-        textabview.setText(profile.getAbout());
-        followingnumview.setText(profile.getFollowing());
-        followersnumview.setText(profile.getFollowers());
-        handleview.setText(profile.getHandle());
-        namelastview.setText(profile.getName() + " " + profile.getLastname());
 
 
         backview = findViewById(R.id.backview);
@@ -71,4 +84,31 @@ public class UserProfileActivity extends AppCompatActivity {
                 .apply(RequestOptions.circleCropTransform())
                 .into(fotoview);
     }
+
+    private void updateProfile() {
+        textabview.setText(profile.getAbout());
+        followingnumview.setText(profile.getFollowing());
+        followersnumview.setText(profile.getFollowers());
+        handleview.setText(profile.getHandle());
+        namelastview.setText(profile.getName() + " " + profile.getLastname());
+    }
+
+    //------------------------------------------------------------------
+    private void jsonParse() {
+        String url="https://api.myjson.com/bins/1der7k";
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                profile = gson.fromJson(response, Profile.class);
+                updateProfile();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
+//------------------------------------------------------------------
 }
